67fe3a44eda7ef239991fe6e77fff0a7c66d360a
sdroszel <57762591+sdroszel@users.noreply.github.com>
2022-02-13 20:44:07 -0500

    Tightened up

--- ColatzConjecture/ColatzConjecture/Program.cs
+++ ColatzConjecture/ColatzConjecture/Program.cs
@@ -5,56 +5,61 @@ namespace ColatzConjecture
 {
     class Program
     {
+        static bool isQuit = false;
+
         static void Main(string[] args)
         {
-            bool isQuit = false;
+            
 
             while (!isQuit)
             {
                 SolveConjecture();
-                isQuit = Quit();
             }
         }
 
         private static void SolveConjecture()
         {
-            Console.Write("\nEnter any positive integer greater than 1: ");
+            Console.Write("\nEnter any positive integer greater than 1 or type QUIT to exit: ");
             string userInput_str = Console.ReadLine();
-            int n = int.Parse(userInput_str);
+            userInput_str = userInput_str.ToLower();
 
-            int steps = 0;
-
-            while (n > 1)
+            if (userInput_str == "quit")
             {
-                if (n % 2 == 0)
-                {
-                    n /= 2;
-                    steps++;
-                }
-                else
-                {
-                    n *= 3;
-                    n += 1;
-                    steps++;
-                }
-            }
-
-            Console.WriteLine("Number of steps: " + steps);
-        }
-
-        private static bool Quit()
-        {
-            Console.Write("\nEnter QUIT to exit or ENTER to test new number: ");
-            string quit = Console.ReadLine();
-            quit = quit.ToLower();
-            if (quit == "quit")
-            {
-                return true;
+                isQuit = true;
             }
             else
             {
-                return false;
+                try
+                {
+                    int n = int.Parse(userInput_str);
+
+                    int steps = 0;
+
+                    while (n > 1)
+                    {
+                        if (n % 2 == 0)
+                        {
+                            n /= 2;
+                            steps++;
+                        }
+                        else
+                        {
+                            n *= 3;
+                            n += 1;
+                            steps++;
+                        }
+                    }
+                    Console.WriteLine("Number of steps: " + steps);
+                }
+
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Incorrect input. Enter an integer greater than 1.");
+                    SolveConjecture();
+                }
             }
+            
         }
     }
 }
