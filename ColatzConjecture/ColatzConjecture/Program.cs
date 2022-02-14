3b33af71b698c3166bcf1312c6318b083864e522
sdroszel <57762591+sdroszel@users.noreply.github.com>
2022-02-13 23:19:59 -0500

    Cleaned up

--- ColatzConjecture/ColatzConjecture/Program.cs
+++ ColatzConjecture/ColatzConjecture/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Transactions;
 
 namespace ColatzConjecture
 {
@@ -9,21 +8,17 @@ namespace ColatzConjecture
 
         static void Main(string[] args)
         {
-            
-
             while (!isQuit)
             {
                 SolveConjecture();
             }
         }
 
+        /* Method to perform calculations for Colatz Conjecture
+         * Counts how many steps it takes to get to one */
         private static void SolveConjecture()
         {
-            Console.Write("\nEnter any positive integer greater than 1 or type QUIT to exit: ");
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            string userInput_str = Console.ReadLine();
-            Console.ResetColor();
-            userInput_str = userInput_str.ToLower();
+            string userInput_str = GetUserInput();
 
             if (userInput_str == "quit")
             {
@@ -31,40 +26,79 @@ namespace ColatzConjecture
             }
             else
             {
+                // Try to convert unser input to integer
                 try
                 {
-                    int n = int.Parse(userInput_str);
-
-                    int steps = 0;
-
-                    while (n > 1)
-                    {
-                        if (n % 2 == 0)
-                        {
-                            n /= 2;
-                            steps++;
-                        }
-                        else
-                        {
-                            n *= 3;
-                            n += 1;
-                            steps++;
-                        }
-                    }
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Number of steps: " + steps);
-                    Console.ResetColor();
+                    Calculation(userInput_str);
                 }
 
                 catch (Exception)
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Incorrect input. Enter an integer greater than 1.");
-                    Console.ResetColor();
-                    SolveConjecture();
+                    DisplayError();
                 }
             }
-            
+
+        }
+
+        private static string GetUserInput()
+        {
+            // Ask user for input
+            Console.Write("\nEnter any positive integer greater than 1 or type QUIT to exit: ");
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            return Console.ReadLine().ToLower();
+        }
+
+        private static void Calculation(string userInput_str)
+        {
+            int n = int.Parse(userInput_str);
+
+            if (n > 1 && n < int.MaxValue)
+            {
+                int steps = 0;
+
+                while (n > 1)
+                {
+                    if (n % 2 == 0)
+                    {
+                        n /= 2;
+                        steps++;
+                    }
+                    else
+                    {
+                        n *= 3;
+                        n += 1;
+                        steps++;
+                    }
+                }
+
+                DisplayResult(steps);
+            }
+            else
+            {
+                DisplayError();
+            }
+        }
+
+        private static void DisplayResult(int steps)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            // Display result
+            Console.WriteLine("Number of steps: " + steps);
+
+            Console.ResetColor();
+        }
+
+        private static void DisplayError()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            // Warn user of incorrect input
+            Console.WriteLine("Incorrect input. Enter an integer greater than 1.");
+
+            Console.ResetColor();
         }
     }
 }
